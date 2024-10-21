46f56f225a02fa9bbcbb276032db992243e16c94
dappon4 <dappon.04@gmail.com>
2024-10-20 22:12:40 -0400

    added blur for better input

--- Pytorch101/app.py
+++ Pytorch101/app.py
@@ -2,9 +2,9 @@ import streamlit as st
 import numpy as np
 import torch
 import torch.nn.functional as F
-from PIL import Image
 from streamlit_drawable_canvas import st_canvas
 import io
+import cv2
 
 # Define the canvas size (28x28 grid for MNIST)
 canvas_size = 28
@@ -27,6 +27,14 @@ def predict_digit(input_image):
         predicted_digit = torch.argmax(probabilities, dim=1).item()
 
     return predicted_digit, probabilities[0,predicted_digit]
+
+def sample_image(gray_image):
+    result_image = np.zeros((28, 28))
+    for i in range(28):
+        for j in range(28):
+            result_image[i, j] = np.mean(gray_image[i*10:(i+1)*10, j*10:(j+1)*10])
+    
+    return result_image
 
 # Main Streamlit app function
 def main():
@@ -53,20 +61,27 @@ def main():
         # Step 2: Process the drawing for prediction
         if canvas_result.image_data is not None:
             # The canvas returns an RGBA image, so we need to process it
-            canvas = np.array(canvas_result.image_data)[:, :, :3]  # Convert RGBA to RGB
-            canvas_gray = np.mean(canvas, axis=2) /255.0 # Convert RGB to grayscale # Normalize pixel values to 0-1
-            canvas_gray = np.clip(canvas_gray, 0.0, 1.0)  # Clip values to 0-1
-            canvas_gray = np.array(Image.fromarray(canvas_gray).convert("L").resize((28, 28)))
+            canvas = np.array(canvas_result.image_data)
+            canvas = cv2.GaussianBlur(canvas, (51, 51), 0)  # Apply Gaussian blur to the image
+            canvas_rgb = canvas[:, :, :3]
+            np.set_printoptions(threshold=np.inf)
+            
+            canvas_gray = np.mean(canvas_rgb, axis=2) /255.0 # Convert RGB to grayscale # Normalize pixel values to 0-1
+            canvas_gray = canvas_gray[..., np.newaxis]
+            canvas_gray = np.squeeze(canvas_gray)
+            canvas_gray = sample_image(canvas_gray)
+            
             input_image = torch.Tensor(canvas_gray)
 
             prediction, probability = predict_digit(input_image)
             st.success(f"Predicted Digit: {prediction}, Probability: {probability.item()*100:.2f}%")
             
-            st.image(canvas_gray * 255, width=canvas_size, caption="Your Drawing (28x28)", use_column_width=True)
+            enlarged_canvas = np.kron(canvas_gray, np.ones((10, 10)))
+            st.image(enlarged_canvas, width=canvas_pixel_size, caption="Your Drawing (28x28)", use_column_width=True)
+            
     else:
-        uploaded_file = st.file_uploader("Upload your JIT-compiled PyTorch model", type=["pt"])
+        uploaded_file = st.file_uploader("Upload your PyTorch model", type=["pt"])
         if uploaded_file:
-            # Load the model and store it in the session state
             st.session_state.model = torch.jit.load(io.BytesIO(uploaded_file.read()))
             st.rerun()  # Reload the app to show the canvas
 
